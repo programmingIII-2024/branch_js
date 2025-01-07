a4c74a9720eccb23530d0b2f82d76ba6338578ec
ohba <ohba@sendai-nct.ac.jp>
2025-01-07 18:42:02 +0900

    First commit

---
+++ branch.js
@@ -0,0 +1,38 @@
+function branch_print()
+{
+	let num = 3;
+
+	// if文の挙動
+	if(num == 3)	// 条件をTrue, Flase, 1, 0と変えてみる
+	{
+		alert("True");
+	}
+	else
+	{
+		alert("False");
+	}
+
+	// swtich文の挙動(長いので省略、C,Javaと同様)
+
+
+	// for文の挙動
+	/*
+	let i;
+	for(i=0; i<3; i++)
+	{
+		alert(i);
+	}
+	*/
+
+	/*
+	let array=["One", "Two", "Three"];
+	for(i=0; i< array.length; i++)		//	for(i in array) と書いても良い
+	{
+		alert(array[i]);
+	}
+	*/
+	
+	// while, break, continueなどCやJavaと同様
+
+
+}
